c1b751f0b71a3792541b4a41ae710d68b5237422
Vivek Karunai Kiri Ragavan <60690810+vivekk16@users.noreply.github.com>
2024-05-02 12:40:08 -0400

    Update bootstrap_master.sh

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -16,20 +16,31 @@ chown -R vagrant:vagrant /home/vagrant/.kube
 #kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml >/dev/null
 #kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml >/dev/null
 
-#echo "[TASK 3] Install Helm"
-#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-#chmod 700 get_helm.sh
-#./get_helm.sh
+echo "[TASK 3] Install Cilium CLI"
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
-#echo "[TASK 4] Deploy Cilium"
+echo "[TASK 4] Install Helm"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+#echo "[TASK 4a] Deploy Cilium with Helm"
 #helm repo add cilium https://helm.cilium.io/
 #helm repo update
-#helm install cilium cilium/cilium --version 1.11.1 --namespace kube-system \
-   # --set hubble.relay.enabled=true \
-   # --set hubble.ui.enabled=true
-#kubectl port-forward -n kube-system svc/hubble-ui 8080:80
-
+#helm install cilium cilium/cilium --version 1.12.19 \
+  # --namespace kube-system \
+  # --set prometheus.enabled=true \
+  # --set operator.prometheus.enabled=true \
+  # --set hubble.enabled=true \
+  # --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
 
 echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
-#mkdir /srv/join
 kubeadm token create --print-join-command > /srv/join/joincluster.sh
